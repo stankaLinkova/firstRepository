31129b2e35e2cb55537736ba60a24d8f186cd8ae
student <student@158.197.35.92>
2018-09-19 07:33:26 +0200

    all of it

--- src/main/java/week1/week1/HelloWorld.java
+++ src/main/java/week1/week1/HelloWorld.java
@@ -2,9 +2,13 @@ package week1.week1;
 
 public class HelloWorld {
 
-	 public static void main( String[] args )
-	    {
-		 System.out.println( "Hello World!" ); 
-	    }
-	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Git je mega");
+		}
+	}
+
 }
